b32cf73df9c2b60c66a3f75d6b3ada53b69de9aa
pbeliveau <pbeliveau@pm.me>
2019-02-19 22:57:54 -0500

    New aliases and functions.

--- pkg/editor-functions.el
+++ pkg/editor-functions.el
@@ -36,3 +36,25 @@
   (insert
    (capitalize
     (s-join " " (s-split-words (file-name-sans-extension (buffer-name)))))))
+
+(eval-after-load "tramp"
+  '(progn
+     (defvar sudo-tramp-prefix
+       "/sudo::"
+       (concat "Prefix to be used by sudo commands when building tramp path "))
+
+     (defun sudo-file-name (filename) (concat sudo-tramp-prefix filename))
+     (defun sudo-reopen-file ()
+       "Reopen file as root by prefixing its name with sudo-tramp-prefix and by
+        clearing buffer-read-only"
+       (interactive)
+       (let*
+           ((file-name (expand-file-name buffer-file-name))
+            (sudo-name (sudo-file-name file-name)))
+         (progn
+           (setq buffer-file-name sudo-name)
+           (rename-buffer sudo-name)
+           (setq buffer-read-only nil)
+           (message (concat "Set file name to " sudo-name)))))
+
+     (global-set-key (kbd "M-g s") 'sudo-reopen-file)))
