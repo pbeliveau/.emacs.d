cffe32a5fc6277cd9f522c7bfd8d7bc829325244
pbeliveau <pbeliveau@pm.me>
2019-03-22 22:39:14 -0400

    Reinstate circadian but keep disable for Win machines.

--- pkg/editor-ui.el
+++ pkg/editor-ui.el
@@ -33,30 +33,25 @@
 (setq default-frame-alist '((font . "Consolas:pixelsize=13")))
 
 ;; theme
-;; (use-package circadian
-;;   :disabled t
-;;   :if (display-graphic-p)
-;;   :ensure t
-;;   :init
-;;   (use-package tao-theme :ensure t :defer t)
-;;   (use-package spacemacs-theme
-;;     :ensure t
-;;     :defer t
-;;     :init
-;;     (setq spacemacs-theme-org-height    nil
-;;           spacemacs-theme-org-highlight nil
-;;           spacemacs-theme-org-bold      nil))
-;;   (setq calendar-latitude   45.41
-;;         calendar-longitude -75.69)
-;;   :config
-;;   (setq circadian-themes '((:sunrise . tao-yang)
-;;                            (:sunset  . spacemacs-dark)))
-;;   (circadian-setup))
-
-(use-package tao-theme
+(use-package circadian
+  :if (display-graphic-p)
+  :if (not (memq window-system '(w32)))
   :ensure t
+  :init
+  (use-package tao-theme :ensure t :defer t)
+  (use-package spacemacs-theme
+    :ensure t
+    :defer t
+    :init
+    (setq spacemacs-theme-org-height    nil
+          spacemacs-theme-org-highlight nil
+          spacemacs-theme-org-bold      nil))
+  (setq calendar-latitude   45.41
+        calendar-longitude -75.69)
   :config
-  (load-theme 'tao-yang t))
+  (setq circadian-themes '((:sunrise . tao-yang)
+                           (:sunset  . spacemacs-dark)))
+  (circadian-setup))
 
 (use-package whitespace
   :ensure nil
