52f3a227d78821cb4eacc0dcf45b0447d500b928
Philippe Beliveau <pbeliveau@pm.me>
2019-04-24 14:52:52 -0400

    Create editor-eww.el

---
+++ etc/editor-eww.el
@@ -0,0 +1,18 @@
+(use-package eww
+  :ensure nil
+  :after shr
+  :bind (:map eww-mode-map
+              ("I" . my/eww-toggle-images))
+  :config
+  (defun my/eww-toggle-images ()
+    (interactive)
+    (setq-local shr-inhibit-images (not shr-inhibit-images))
+    (eww-reload t)
+    (message "Images are now %s"
+             (if shr-inhibit-images "off" "on"))))
+
+(use-package shr
+  :ensure nil
+  :init
+  (setq-default shr-inhibit-images t
+                shr-use-fonts      nil))
