656d1083cf8c4b879f276b50a7a58f575f2f430b
pbeliveau <pbeliveau@pm.me>
2019-07-04 23:38:09 -0400

    Add solaire-mode for theme that supports it.

--- etc/editor-ui.el
+++ etc/editor-ui.el
@@ -55,6 +55,14 @@
 (if (memq window-system '(w32))
     (load-theme 'almost-mono-white t))
 
+(use-package solaire-mode
+  :hook
+  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+  (minibuffer-setup . solaire-mode-in-minibuffer)
+  :config
+  (solaire-global-mode +1)
+  (solaire-mode-swap-bg))
+
 (use-package page-break-lines
   :ensure t
   :diminish
