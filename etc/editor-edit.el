c28dee406feb8c396b5ed1345b6b5be84c8274e8
Philippe Beliveau <pbeliveau@pm.me>
2020-05-13 15:22:21 -0400

    + visual-regexp

--- etc/editor-edit.el
+++ etc/editor-edit.el
@@ -33,6 +33,8 @@
                      :host github
                      :repo "hatheroldev/mc-calc"))
 
+(use-package visual-regexp)
+
 (prefer-coding-system 'utf-8)
 (set-charset-priority 'unicode)
 (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
